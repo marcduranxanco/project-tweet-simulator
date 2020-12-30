88a13c158ed0f84806b07f7ec2f6d9d9bb14e2c5
MarcDuran <marcduranxanco@hotmail.com>
2020-12-30 16:54:46 +0100

    UPDATE: Save tweets to local storage

--- src/components/SendTweet/SendTweet.js
+++ src/components/SendTweet/SendTweet.js
@@ -4,6 +4,7 @@ import AddIcon from '@material-ui/icons/Add';
 import moment from 'moment';
 import ModalContainer from '../ModalContainer';
 import FormSendTweet from '../FormSendTweet';
+import { TWEETS_STORAGE } from '../../utils/Constants';
 
 import './SendTweet.scss';
 
@@ -11,11 +12,26 @@ export default function SendTweet() {
     const [ isOpenModal, setIsOpenModal ] = useState(false);
     const openModal = () => {
         setIsOpenModal (true);
-    }
+    };
     const closeModal = () => {
         setIsOpenModal (false);
-    }
+    };
 
+    const sendTweet = (event, formValue) => {
+        event.preventDefault();
+        const { name, tweet}  =formValue;
+        let allTweetsArray = [];
+        if(!name || !tweet) {
+            console.log("WARNING: Todos los campos son obligatorios")
+        } else{
+            formValue.time = moment();
+            allTweetsArray.push(formValue);
+            localStorage.setItem(TWEETS_STORAGE, JSON.stringify(allTweetsArray));
+            console.log("Se ha enviado el Tweet");
+            closeModal();
+        }
+        allTweetsArray = [];
+    };
 
     return (
         <div className="send-tweet">
@@ -24,7 +40,7 @@ export default function SendTweet() {
             </Fab>
 
             <ModalContainer isOpenModal={isOpenModal} closeModal={closeModal}>
-                <FormSendTweet />
+                <FormSendTweet sendTweet={sendTweet}/>
             </ModalContainer>
         </div>
     );
